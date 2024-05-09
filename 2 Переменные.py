e4002ef83439170de4b47b8cf98b3795733a32f3
Zakieva <168937136+Zakieva@users.noreply.github.com>
2024-05-09 10:47:23 +0300

    Add files via upload
    Все практические задания.

---
+++ 2 Переменные.py
@@ -0,0 +1,6 @@
+num1 = 5
+num2 = 3
+_sum = num1 + num2  # внесла изменение в название переменной, что бы убрать Shadows built-in name 'sum'
+print(_sum)
+resalt = _sum + num1 * num2
+print(resalt)
---
+++ 3 Динамическая типизация.py
@@ -0,0 +1,8 @@
+name = 'Ekaterina'  #type - str
+print(name)
+age = 45  #type - int
+print(age)
+age = age + 1 #type - int
+print(age)
+is_student = True  #type - bool
+print(is_student)
