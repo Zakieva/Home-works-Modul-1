48ebd2d2c0daba29d68c58c02afc5147bdbc163d
Zakieva <168937136+Zakieva@users.noreply.github.com>
2024-05-13 11:46:00 +0300

    Add files via upload

---
+++ 6 Списки и словари.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'bananas', 'peach', 'pineapple', 'pear']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:])
+my_list[2] = 'surprise'
+print(my_list)
+my_dict = {'xue2 xi2': 'учиться', 'lao3 shi1': 'учитель', 'han4 yu1': 'китайский язык'}
+print(my_dict)
+print(my_dict['xue2 xi2'])
+my_dict['xue2 xi2'] = 'учить, изучать'
+print(my_dict)
+
